9e842ced43dd4eb6ba615350b395713e4de7ce99
Ruslan <botyrovra@gmail.com>
2022-06-15 22:23:21 +0300

    add io_helper.py

--- src/io_helper.py
+++ src/io_helper.py
@@ -1,19 +1,14 @@
+# Метод read_text_from_file(file_name) считывает данные с файла, закрывает его и возвращает считанные данные
 
-# Добавить метод read_text_from_file(file_name)
-#Алгоритм:
-#Считывает данные с файла file_name
-#Закрывает файл
-#Возвращает считанные с файла данные
+def read_text_from_file(file_name):
+    data = open(file_name)
+    text_from_file = data.read()
+    data.close
+    return text_from_file
+    
+# Метод write_text_in_file(file_name, text) открывает файл, записывает текст и закрывает его
 
-
-
-
-
-
-
-
-
-# Добавить метод write_text_in_file(file_name, text)
-#Алгоритм:
-#Записывает text в файл file_name
-#Закрывает файл
+def write_text_in_file(file_name, text):
+    data = open(file_name, 'a')
+    data.writelines('\n' + text)
+    data.close
