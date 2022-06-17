9119896504e421528aa961a50516473f7d4547cc
Savin <savinn1989@mail.ru>
2022-06-17 21:26:13 +1000

    add parsing metods

---
+++ src/private/parsing_format.py
@@ -0,0 +1,22 @@
+from core.format import FORMATS
+
+def parse_user_properties_by_delimeter(properties, user_properties_delimeter):
+    list_properties = properties.split(user_properties_delimeter)
+    return list_properties
+ 
+
+def parse_user_data_by_delimeter(users_data, user_data_delimeter, user_properties_delimeter):
+    parsed_users_data = []
+    list_properties = users_data.split(user_data_delimeter)
+    for data in list_properties:
+        parsed_users_data.append(parse_user_properties_by_delimeter(data, user_properties_delimeter))
+    return parsed_users_data
+
+
+def parse_format_data_by_delimeter(format_data, format_id):
+    temp_list = format_data.split(FORMATS[format_id]['formatDataDelimeter'])
+    print(temp_list)
+    parsed_format_data = []
+    for data in temp_list:
+        parsed_format_data.append(parse_user_data_by_delimeter(data, FORMATS[format_id]['userDataDelimeter'], FORMATS[format_id]['userPropertiesDelimeter']))
+    return parsed_format_data
