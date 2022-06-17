695f32a4fafec3d1d43f0867453c747695623593
Savin <savinn1989@mail.ru>
2022-06-17 21:35:02 +1000

    add import dic

--- src/private/parsing_format.py
+++ src/private/parsing_format.py
@@ -1,4 +1,4 @@
-from core.format import FORMATS
+from src.core.format import FORMATS
 
 def parse_user_properties_by_delimeter(properties, user_properties_delimeter):
     list_properties = properties.split(user_properties_delimeter)
@@ -15,7 +15,6 @@ def parse_user_data_by_delimeter(users_data, user_data_delimeter, user_propertie
 
 def parse_format_data_by_delimeter(format_data, format_id):
     temp_list = format_data.split(FORMATS[format_id]['formatDataDelimeter'])
-    print(temp_list)
     parsed_format_data = []
     for data in temp_list:
         parsed_format_data.append(parse_user_data_by_delimeter(data, FORMATS[format_id]['userDataDelimeter'], FORMATS[format_id]['userPropertiesDelimeter']))
