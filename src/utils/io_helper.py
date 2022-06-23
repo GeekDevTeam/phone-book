82486af576dfe41cd8196de51d70871933128216
Xenia <listk98@yandex.ru>
2022-06-23 16:10:59 +0300

    Add encoding in io_helper

--- src/utils/io_helper.py
+++ src/utils/io_helper.py
@@ -1,10 +1,10 @@
 # Метод считывает и возвращает считанные данные с файла file_name
 def read_text_from_file(file_name):
-    with open(file_name, 'r') as file:
+    with open(file_name, 'r', encoding='utf-8') as file:
         data = file.read()
     return data
 
 # Метод позволяет записать текст в файл
 def write_text_in_file(file_name, text):
-    with open(file_name, 'w') as file:
+    with open(file_name, 'w', encoding='utf-8') as file:
         file.write(text)
