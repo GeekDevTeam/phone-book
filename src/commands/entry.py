4c156f4d77daa60c4ddf98a3ba3ccaaebf050347
Savin <savinn1989@mail.ru>
2022-06-25 10:47:47 +1000

    add_show_command

--- src/commands/entry.py
+++ src/commands/entry.py
@@ -5,5 +5,6 @@ commands = {
     'start': { # command_name - название команды для телеграм бота
         'command_handler': start_command, # обработчик команды
         'description': 'Отправляет приветственное сообщение' # описание команды
-    }
+    },
+    'show': {'comand_handler': show_command, 'description': 'Сообщает данные пользователя из справочника'}
 }
