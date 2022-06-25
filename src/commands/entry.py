cca0c8355e2189567b85a95a7ddf0e0391e74ad6
AdilBikeev <bikeev1999@mail.ru>
2022-06-25 03:45:29 +0300

    Add help_command

--- src/commands/entry.py
+++ src/commands/entry.py
@@ -1,3 +1,4 @@
+from commands.help_command import help_command
 from commands.start_command import start_command
 from commands.convert_to_command import convert_to_command
 # Словарь команд телеграм бота
@@ -6,8 +7,12 @@ commands = {
         'command_handler': start_command,  # обработчик команды
         'description': 'Отправляет приветственное сообщение'  # описание команды
     },
-    'convert_to': {  # command_name - название команды для телеграм бота
-        'command_handler': convert_to_command,  # обработчик команды
-        'description': 'Преобразование формата хранения данных'  # описание команды
-    }
+    'convert_to': {
+        'command_handler': convert_to_command,
+        'description': "Преобразование формата хранения данных в заданный formatId. Пример использования: '/convert_to {formatId}'"
+    },
+    'help': {
+        'command_handler': help_command,
+        'description': 'Отображает список доступных команд'
+    },
 }
